09ca4d474be8d97e3d6c21a88b10391ed3cae903
Nick Reuter <nreuter85@gmail.com>
2026-07-14 16:36:33 -0500

    genevn: Drop MotCamera3
    Change-Id: Idd2416f9a1259e9e7caee30fbd4db011d1c3f296

--- genevn-vendor.mk
+++ genevn-vendor.mk
@@ -3111,18 +3111,10 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/genevn/proprietary/product/etc/CarrierSettings/s90158.pb:$(TARGET_COPY_OUT_PRODUCT)/etc/CarrierSettings/s90158.pb
 
 PRODUCT_COPY_FILES += \
-    vendor/motorola/genevn/proprietary/product/etc/permissions/com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.camera3.xml \
-    vendor/motorola/genevn/proprietary/product/etc/permissions/com.motorola.moto-uirefresh.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.motorola.moto-uirefresh.xml \
-    vendor/motorola/genevn/proprietary/product/etc/permissions/deviceowner-configuration-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/deviceowner-configuration-com.motorola.camera3.xml \
-    vendor/motorola/genevn/proprietary/product/etc/permissions/privapp-permissions-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-com.motorola.camera3.xml \
-    vendor/motorola/genevn/proprietary/product/etc/sysconfig/hiddenapi-whitelist-com.motorola.camera3.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/hiddenapi-whitelist-com.motorola.camera3.xml \
     vendor/motorola/genevn/proprietary/recovery/root/vendor/firmware/ILITEK_FW:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/ILITEK_FW \
     vendor/motorola/genevn/proprietary/recovery/root/vendor/firmware/NT36xxx_MP_Setting_Criteria_605B.csv:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/NT36xxx_MP_Setting_Criteria_605B.csv \
     vendor/motorola/genevn/proprietary/recovery/root/vendor/firmware/csot_novatek_ts_fw.bin:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/csot_novatek_ts_fw.bin \
     vendor/motorola/genevn/proprietary/recovery/root/vendor/firmware/csot_novatek_ts_mp.bin:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/csot_novatek_ts_mp.bin \
-    vendor/motorola/genevn/proprietary/system/etc/permissions/com.motorola.motosignature.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/com.motorola.motosignature.xml \
-    vendor/motorola/genevn/proprietary/system/etc/permissions/moto-core_services.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/moto-core_services.xml \
-    vendor/motorola/genevn/proprietary/system/etc/permissions/moto-settings.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/moto-settings.xml \
     vendor/motorola/genevn/proprietary/system_ext/etc/permissions/com.android.hotwordenrollment.common.util.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/com.android.hotwordenrollment.common.util.xml \
     vendor/motorola/genevn/proprietary/vendor/etc/acdbdata/nn_ns_models/fai__2.0.0_0.1__3.0.0_0.0__eai_1.36_enpu2.pmd:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/nn_ns_models/fai__2.0.0_0.1__3.0.0_0.0__eai_1.36_enpu2.pmd \
     vendor/motorola/genevn/proprietary/vendor/etc/acdbdata/nn_ns_models/fai__2.3.0_0.1__3.0.0_0.0__eai_1.10.pmd:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/nn_ns_models/fai__2.3.0_0.1__3.0.0_0.0__eai_1.10.pmd \
@@ -3154,10 +3146,6 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/genevn/proprietary/vendor/etc/libnfc-PN560.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-PN560.conf \
     vendor/motorola/genevn/proprietary/vendor/etc/libnfc-SN220-UICC.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-SN220-UICC.conf \
     vendor/motorola/genevn/proprietary/vendor/etc/libnfc-nci_PN560.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci_PN560.conf \
-    vendor/motorola/genevn/proprietary/vendor/etc/permissions/com.motorola.camera3.content.ai.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.content.ai.xml \
-    vendor/motorola/genevn/proprietary/vendor/etc/permissions/com.motorola.camera3.genevn.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.genevn.xml \
-    vendor/motorola/genevn/proprietary/vendor/etc/permissions/com.motorola.camera3.lens.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.lens.xml \
-    vendor/motorola/genevn/proprietary/vendor/etc/permissions/com.motorola.camera3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.motorola.camera3.xml \
     vendor/motorola/genevn/proprietary/vendor/etc/sensors/config/bmi3x0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/bmi3x0.json \
     vendor/motorola/genevn/proprietary/vendor/etc/sensors/config/bmp5_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/bmp5_0.json \
     vendor/motorola/genevn/proprietary/vendor/etc/sensors/config/icm4x6xa.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/icm4x6xa.json \
@@ -3464,19 +3452,11 @@ PRODUCT_PACKAGES += \
     vendor.egistecbix.hardware.fingerprint@4.0 \
     vendor.qti.hardware.camera.postproc@1.0-service-impl \
     vendor.qti.hardware.fingerprint@1.0 \
-    MotoSignatureApp \
-    MotCamera3AI \
     HotwordEnrollmentOKGoogleHEXAGON_WIDEBAND \
     HotwordEnrollmentXGoogleHEXAGON_WIDEBAND \
-    MotCamera4 \
-    MotorolaSettingsProvider \
-    com.motorola.motosignature \
-    moto-core_services \
-    moto-settings \
     com.android.hotwordenrollment.common.util \
     capsense_reset \
     android.hardware.biometrics.fingerprint@2.1-service-ets \
     android.hardware.biometrics.fingerprint@2.1-service-fpc \
     fpc_ident \
-    vendor.qti.camera.provider@2.7-service_64 \
-    motsettings
+    vendor.qti.camera.provider@2.7-service_64
